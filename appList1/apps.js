5ed199513e2bde2d50b177c2dd8f47511f2cb259
Asicosilomu <alexandruarapu04@gmail.com>
2023-12-06 14:53:36 +0200

    Upload appList1

---
+++ appList1/apps.js
@@ -0,0 +1,328 @@
+ window.Apps = [
+     {
+         "label": "View video",
+         "apk": "ViewVideo.apk",
+         "desc": "Video Player",
+         "icon": "icon_launcher_video.png",
+         "checked": true
+     },
+     {
+         "label": "View video",
+         "apk": "ViewVideo2.apk",
+         "desc": "Alternative apk of video player",
+         "icon": "icon_launcher_video.png",
+     },
+     {
+         "label": "Calendar",
+         "apk": "Calendar.apk",
+         "desc": "HTC Calendar",
+         "icon": "icon_launcher_a_calendar.webp",
+         "checked": true
+     },
+     {
+         "label": "Location Picker",
+         "apk": "LocationPicker.apk",
+         "desc": "App allows you to pick GPS location from other apps, e.g. calendar",
+         "icon": "icon_launcher_location_picker.png",
+         "checked": true
+     },
+     {
+         "label": "com.htc.framework",
+         "apk": "com.htc.framework.apk",
+         "desc": "HTC Framework. May be needed for some apps",
+         "icon": "default.png",
+         "required": true
+     },
+     {
+         "label": "HTC Account",
+         "apk": "HTCAccount.apk",
+         "desc": "HTC account manager. may not work.",
+         "icon": "icon_launcher_htc.png"
+     },
+     {
+         "label": "com.htc.drawingboard",
+         "apk": "com.htc.drawingboard.apk",
+         "desc": "Not sure what this is supposed to be.",
+         "icon": "default.png"
+     },
+     {
+         "label": "Ringtone Trimmer",
+         "apk": "RingtoneTrimmer.apk",
+         "desc": "Ringtone Trimmer",
+         "icon": "icon_launcher_ringtone_trimmer.webp"
+     },
+     {
+         "label": "E-mail",
+         "apk": "Email.apk",
+         "desc": "HTC email client. Supports IMAP accounts, Gmail (via OAuth) and Exchange ActiveSync.",
+         "icon": "icon_launcher_mail.png",
+         "checked": true
+     },
+     {
+         "label": "Gallery",
+         "apk": "Gallery10.apk",
+         "desc": "HTC Gallery",
+         "icon": "icon_launcher_albums.png",
+         "checked": true
+     },
+     {
+         "label": "Clock",
+         "apk": "Clock.apk",
+         "desc": "HTC Clock. Alarms don't work",
+         "icon": "icon_launcher_world_clock.png",
+         "checked": true
+     },
+     {
+         "label": "HTC Connect",
+         "apk": "HTCConnect.apk",
+         "desc": "HTC Connect, used for casting media to other devices. Probably won't work.",
+         "icon": "icon_launcher_tube_setting.webp"
+     },
+     {
+         "label": "Dot Breaker",
+         "apk": "DotBreaker.apk",
+         "desc": "Little game that you can play with the HTC dot view case. Can't get it to work, but you can try your luck.",
+         "icon": "dot_breaker_launcher.png"
+     },
+     {
+         "label": "Scribble",
+         "apk": "Scribble.apk",
+         "desc": "Cool note-taking app with plenty of features.",
+         "icon": "icon_launcher_scribble.png",
+         "checked": true
+     },
+     {
+         "label": "HTC Sense Input",
+         "apk": "SenseInput.apk",
+         "desc": "HTC Sense virtual keyboard. You can install the language packs for your languages to use autocorrect. Language packs that aren't in here can be downloaded from Apkmirror.",
+         "icon": "icon_launcher_keyboard.png",
+         "checked": true
+     },
+     {
+         "label": "HTC Service - DLNA",
+         "apk": "DLNAService.apk",
+         "desc": "DLNA Service",
+         "icon": "icon_launcher_dlna.png"
+     },
+     {
+         "label": "HTC Sync Manager",
+         "apk": "SyncManager.apk",
+         "desc": "HTC Sync Manager",
+         "icon": "app_icon.png"
+     },
+     {
+         "label": "HTC Sync Manager Package Installer",
+         "apk": "SyncManagerPackageInstaller.apk",
+         "desc": "HTC Sync Manager",
+         "icon": "app_icon.png"
+     },
+     {
+         "label": "Keyboard - English Pack",
+         "apk": "LangPackEN.apk",
+         "desc": "Install this app to get English language support in HTC Sense Input.",
+         "icon": "icon_launcher_langpack.png",
+         "checked": true
+     },
+     {
+         "label": "Keyboard - French Pack",
+         "apk": "LangPackFR.apk",
+         "desc": "Install this app to get French language support in HTC Sense Input.",
+         "icon": "icon_launcher_langpack.png"
+     },
+     {
+         "label": "Keyboard - Romanian Pack",
+         "apk": "LangPackRO.apk",
+         "desc": "Install this app to get Romanian language support in HTC Sense Input.",
+         "icon": "icon_launcher_langpack.png"
+     },
+     {
+         "label": "Keyboard - Handwriting Pack",
+         "apk": "LangPackHandwriting.apk",
+         "desc": "Handwriting langpack.",
+         "icon": "icon_launcher_langpack.png"
+     },
+     {
+         "label": "Flashlight",
+         "apk": "Flashlight.apk",
+         "desc": "HTC branded flashlight app.",
+         "icon": "icon_launcher_a_flashlight.png",
+         "checked": true
+     },
+     {
+         "label": "Weather",
+         "apk": "Weather.apk",
+         "desc": "HTC Weather app. Pulls data from AccuWeather.",
+         "icon": "icon_launcher_weather.png",
+         "checked": true
+     },
+     {
+         "label": "Wi-Fi Hotspot",
+         "apk": "WifiHotspot.apk",
+         "desc": "Wifi Hotspot",
+         "icon": "icon_launcher_wifi_hotspot.png"
+     },
+     {
+         "label": "Photo enhancer",
+         "apk": "PhotoEnhancer2.apk",
+         "desc": "Photo enhancer",
+         "icon": "icon_launcher_photo_editor.webp"
+     },
+     {
+         "label": "HTC Service Pack",
+         "apk": "ServicePack.apk",
+         "desc": "HTC Service Pack. Required by some apps e.g. weather",
+         "icon": "icon_launcher_update.png",
+         "required": true
+     },
+     {
+         "label": "Location Services",
+         "apk": "LocationServices.apk",
+         "desc": "HTC Location Services",
+         "icon": "icon_category_pin_location.png",
+     },
+     {
+         "label": "Speak",
+         "apk": "Speak.apk",
+         "desc": "HTC Speak. It doesn't work (at least for me) as it wants to download something from a dead server.",
+         "icon": "icon_launcher_speak.png",
+     },
+     {
+         "label": "Sound Picker",
+         "apk": "SoundPicker.apk",
+         "desc": "Ringtone picker",
+         "icon": "icon_launcher_folder_sound_set.png",
+         "checked": true
+     },
+     {
+         "label": "Twitter for HTC Sense",
+         "apk": "HtcSenseTwitter.apk",
+         "desc": "Adds Twitter feed integration to BlinkFeed. It worked last time I tried it (late 2022).",
+         "icon": "icon_launcher_twitter.png",
+         "checked": true
+     },
+     {
+         "label": "Tasks",
+         "apk": "Tasks.apk",
+         "desc": "A simple to-do app. Should be able to sync to Google tasks.",
+         "icon": "icon_launcher_todo_task.png",
+         "checked": true
+     },
+     {
+         "label": "Archive Viewer",
+         "apk": "ArchiveViewer.apk",
+         "desc": "Very basic zip archive viewer.",
+         "icon": "default.png",
+         "checked": true
+     },
+     {
+         "label": "File Manager",
+         "apk": "FileManager.apk",
+         "desc": "File manager.",
+         "icon": "icon_launcher_folder.png",
+         "checked": true
+     },
+     {
+         "label": "People Widget",
+         "apk": "ContactWidget.apk",
+         "desc": "Contacts widget. Doesn't work unless you have HTC people",
+         "icon": "icon_launcher_people.png"
+     },
+     {
+         "label": "Bluetooth Widget",
+         "apk": "BluetoothWidget.apk",
+         "desc": "Bluetooth toggle widget",
+         "icon": "icon_launcher_bluetooth.png",
+         "checked": true
+     },
+     {
+         "label": "Calculator Widget",
+         "apk": "CalculatorWidget.apk",
+         "desc": "A full sized calculator in a widget",
+         "icon": "icon_launcher_calculator.png",
+         "checked": true
+     },
+     {
+         "label": "GPS Widget",
+         "apk": "GPSWidget.apk",
+         "desc": "GPS Widget",
+         "icon": "icon_launcher_gps.png",
+         "checked": true
+     },
+     {
+         "label": "Weather Clock Widget",
+         "apk": "WeatherClockWidget.apk",
+         "desc": "A couple of types of weather clock widget, including the infamous flip clock.",
+         "icon": "icon_launcher_world_clock.png",
+         "checked": true
+     },
+     {
+         "label": "Transparent Weather Clock Widget",
+         "apk": "WeatherClockTransWidget.apk",
+         "desc": "Transparent Weather Clock Widget.",
+         "icon": "icon_launcher_world_clock_trans.png",
+         "checked": true
+     },
+     {
+         "label": "Data roaming Widget",
+         "apk": "DataRoamingWidget.apk",
+         "desc": "Data roaming Widget",
+         "icon": "icon_launcher_roaming.png"
+     },
+     {
+         "label": "Screen Brightness Widget",
+         "apk": "BrightnessWidget.apk",
+         "desc": "Screen Brightness Widget",
+         "icon": "icon_launcher_brightness.png",
+         "checked": true
+     },
+     {
+         "label": "Hotspot Widget",
+         "apk": "HotspotWidget.apk",
+         "desc": "Hotspot Widget",
+         "icon": "icon_launcher_wifi_hotspot.png"
+     },
+     {
+         "label": "FM Radio Widget",
+         "apk": "FMRadioWidget.apk",
+         "desc": "FM Radio Widget. No work",
+         "icon": "icon_launcher_fm_radio.png"
+     },
+     {
+         "label": "Ringtone Widget",
+         "apk": "RingtoneWidget.apk",
+         "desc": "Ringtone Widget",
+         "icon": "icon_launcher_ringtone.png"
+     },
+     {
+         "label": "Auto sync widget",
+         "apk": "AutoSyncWidget.apk",
+         "desc": "Auto sync widget",
+         "icon": "icon_launcher_autosync.png"
+     },
+     {
+         "label": "Sync All Widget",
+         "apk": "SyncWidget.apk",
+         "desc": "Sync Widget",
+         "icon": "icon_launcher_sync_all.png"
+     },
+     {
+         "label": "Power Dashboard Widget",
+         "apk": "PowerStripWidget.apk",
+         "desc": "Power Dashboard Widget",
+         "icon": "icon_launcher_powerstrip.png",
+         "checked": true
+     },
+     {
+         "label": "Data Dashboard Widget",
+         "apk": "DataStripWidget.apk",
+         "desc": "Data Dashboard Widget",
+         "icon": "icon_launcher_datastrip.png",
+         "checked": true
+     },
+     {
+         "label": "Wifi Widget",
+         "apk": "WifiWidget.apk",
+         "desc": "Wifi Widget",
+         "icon": "icon_launcher_wifi.png"
+     },
+]
